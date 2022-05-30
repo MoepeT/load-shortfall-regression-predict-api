b0b92ed0adf86dedb0939db7fa882b2b91e28a49
Angela Morris <angela.morris@yahoo.com>
2022-05-30 19:05:43 +0200

    Update request.py

--- utils/request.py
+++ utils/request.py
@@ -45,7 +45,7 @@ feature_vector_json = test.iloc[1].to_json()
 # replace the URL below with its public IP:
 
 # url = 'http://3.251.90.89:5000/api_v0.1'
-url = 'http://3.251.90.89:5000/api_v0.1'
+url = 'http://54.72.214.68:5000/api_v0.1'
 
 # Perform the POST request.
 print(f"Sending POST request to web server API at: {url}")
